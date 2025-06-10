99915fa8360e0f579257d0fd15262139a4825faa
nikociardit <77273322+nikociardit@users.noreply.github.com>
2025-06-10 19:18:12 +0200

    Create .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+{
+  "extends": [
+    "next/core-web-vitals",
+    "next/typescript"
+  ],
+  "rules": {
+    "@next/next/no-img-element": "off",
+    "react/no-unescaped-entities": "off",
+    "@typescript-eslint/no-unused-vars": "warn",
+    "@typescript-eslint/no-explicit-any": "warn",
+    "prefer-const": "error",
+    "no-var": "error"
+  }
+}
